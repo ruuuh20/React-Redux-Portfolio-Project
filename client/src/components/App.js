39564d3d9bc2d7f6e166e157891a74d988b3e7e3
Nourski <nour.kodeih@gmail.com>
2018-02-14 13:06:07 -0500

    made routes own component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, NavLink, Route } from 'react-router-dom';
-import SearchPage from './SearchPage';
+import Routes from './Routes'
 
 const App = (props) =>
-  <Router>
-    <div>
-      <div style={{ borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}>
-        <NavLink style={{ marginRight: '10px' }} to="/search">Search Listings</NavLink>
-        <NavLink style={{ marginRight: '10px' }} to="/">Home</NavLink>
-      </div>
-      <Route path="/" render={() => <h3>Welcome to the NYC Apartment Search App</h3>} />
-      <Route path="/search" component={SearchPage} />
-    </div>
-  </Router>;
+  <Routes />
 
 export default App;
