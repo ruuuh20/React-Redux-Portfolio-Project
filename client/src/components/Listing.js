5e0913e5b5dfd08dfc5c2cf140d83d813e133b93
Nourski <nour.kodeih@gmail.com>
2018-02-15 17:00:36 -0500

    fixed card margin and padding for featured listings

--- client/src/components/Listing.js
+++ client/src/components/Listing.js
@@ -7,7 +7,7 @@ const Listing = (props) => {
     <div className="card col-sm-2">
       <img className="card-img-top img-fluid" src={listing.main_img} alt={listing.address}/>
       <div className="card-block">
-        test
+        <h4 className="card-title">Card title</h4>
       </div>
     </div>
   )
