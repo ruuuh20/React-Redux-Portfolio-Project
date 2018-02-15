6af0b5981a3c6d59ac4503dbd82058ff881cb645
Nourski <nour.kodeih@gmail.com>
2018-02-15 17:45:12 -0500

    added formatted price to listing model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,6 +4,18 @@ class Listing < ApplicationRecord
   belongs_to :realtor
   accepts_nested_attributes_for :comments, reject_if: :all_blank, allow_destroy: true
 
-  # define scope for featured_listings
+  def listing_price_formatted
+    number = self.listing_price.to_s.split("").unshift("$").reverse.each_slice(3).to_a
+    i = 1
+    price = []
+    until i == number.length
+      price.push(number[number.length - i].reverse)
+      price.push(",")
+      i += 1
+    end
+    price.push(number[0].reverse)
+    price.join("")
+  end
+
 
 end
--- app/serializers/listing_serializer.rb
+++ app/serializers/listing_serializer.rb
@@ -1,3 +1,3 @@
 class ListingSerializer < ActiveModel::Serializer
-  attributes :id, :main_img, :images, :neighborhood, :address, :listing_price, :beds, :baths, :description, :comments
+  attributes :id, :main_img, :images, :neighborhood, :address, :listing_price, :listing_price_formatted, :beds, :baths, :description, :comments
 end
