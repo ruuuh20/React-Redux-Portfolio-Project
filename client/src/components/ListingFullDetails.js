57119726d7c1f8474b60a872b457adc87e6dc281
Nourski <nour.kodeih@gmail.com>
2018-02-16 19:05:06 -0500

    prleliminary styling of ListingFullDetails component

--- client/src/components/ListingFullDetails.js
+++ client/src/components/ListingFullDetails.js
@@ -4,8 +4,26 @@ import React from 'react'
 const ListingFullDetails = ({listing}) => {
 
   return (
-    <div>
-      {listing.address}
+    <div className="container-fluid">
+      <div className="row">
+        <div className= "col-6 images-column">
+          <img className="main-img img-fluid" src={listing.main_img} alt={listing.address}/>
+        </div>
+
+        <div className="col-4">
+          <div className="row">
+            <div className="col-12">
+              <h3 className="listing-title">{listing.address}</h3>
+              <h4 className="listing-detail">{listing.neighborhood}</h4>
+              <h5 className="listing-detail">{listing.listing_price_formatted}</h5>
+              <h6 className="listing-detail">{listing.beds} {listing.beds===1? "bed" : "beds"} / {listing.baths} {listing.baths===1? "bath" : "baths"}</h6>
+              <button type="button" className="btn viewing-button btn-secondary">Request Viewing</button>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
     </div>
 )}
 
