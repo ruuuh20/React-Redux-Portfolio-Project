e98a47f8e2502c1c9d3b1e7d70ee567671a8818b
Nourski <nour.kodeih@gmail.com>
2018-02-17 13:26:51 -0500

    updated search results css

--- client/src/components/FilteredListing.js
+++ client/src/components/FilteredListing.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-const FilteredListing = (props) => {
+const FilteredListing = ({listing}) => {
   return (
-    <div>
-      Hello World
+    <div className="row filtered-listing">
+      <div className="col-6 filtered-listing-img">
+        <img className="img-fluid" src={listing.main_img} alt={listing.address}/>
+      </div>
+      <div className="filtered-listing-details col-6">
+        <h5 className="listing-title">{listing.address}</h5>
+        <p>{listing.neighborhood}</p>
+        <p>{listing.listing_price_formatted}</p>
+        <p>{listing.beds} {listing.beds===1? "bed" : "beds"} / {listing.baths} {listing.baths===1? "bath" : "baths"}</p>
+      </div>
     </div>
   )
 }
