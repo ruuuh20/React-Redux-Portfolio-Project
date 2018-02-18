19aabc8adb80eda54b90a0a775d92621e1b2caa9
Nourski <nour.kodeih@gmail.com>
2018-02-17 20:03:45 -0500

    limit results view to 3 at a time

--- client/src/components/SearchResults.js
+++ client/src/components/SearchResults.js
@@ -1,14 +1,59 @@
-import React from 'react';
+import React, {Component} from 'react';
 import FilteredListing from './FilteredListing'
 
-const SearchResults = ({listings}) => {
-  return (
-    <div>
-      <div className="search-results">
-        {listings.map(listing => <FilteredListing listing = {listing} />)}
+class SearchResults extends Component {
+  constructor() {
+    super()
+    this.state = {
+      listings: [],
+      listingsIndex: 0
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    var listings = []
+    var allListings = nextProps.listings
+    while (allListings.length > 0) {
+      listings.push(allListings.splice(0, 3))
+    }
+    if (allListings.length === 0){
+      this.setState({
+        listings: listings
+      })}
+  }
+
+  handleNext = () => {
+    this.setState({
+      listingsIndex: ++this.state.listingsIndex
+    })
+  }
+
+  handlePrevious = () => {
+    this.setState({
+      listingsIndex: --this.state.listingsIndex
+    })
+  }
+
+  render(){
+    let listings = this.state.listings
+    if (this.state.listings.length >0) {listings = this.state.listings[this.state.listingsIndex]}
+    let backButton
+    if (this.state.listingsIndex !== 0){backButton =
+      <a onClick={this.handlePrevious} className="previous round">&#8249;</a>}
+    let forwardButton
+    if (this.state.listingsIndex < this.state.listings.length - 1){forwardButton =
+      <a onClick={this.handleNext} className="next round">&#8250;</a>}
+    return (
+      <div>
+        <div className="list-buttons">
+          {backButton}
+          {forwardButton}
+        </div>
+        <div className="search-results">
+          {listings.map(listing => <FilteredListing listing = {listing} />)}
+        </div>
       </div>
-    </div>
-  )
+    )}
 }
 
 export default SearchResults
