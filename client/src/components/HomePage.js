5ec3917b0dd29dfd2f154e8903aa8df41e50baa0
Nourski <nour.kodeih@gmail.com>
2018-02-19 16:33:34 -0500

    updated homepage css

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -7,12 +7,12 @@ const HomePage = (props) => {
   const realtors = props.realtors
 
   return (
-    <div>
-      <h1> Featured Listings </h1>
+    <div className="home-page">
+      <h3> Featured Listings </h3>
       <div className="scrolling-wrapper-flexbox">
         {listings.map(listing => <FeaturedListing listing = {listing} />)}
       </div>
-      <h1> Realtors </h1>
+      <h3> Realtors </h3>
       <div className="row realtors">
         {realtors.map(realtor => <RealtorCard realtor = {realtor} />)}
       </div>
