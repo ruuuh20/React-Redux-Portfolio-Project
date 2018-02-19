b6817e81c6d34e13e0314d2b31c64dc8a363e013
Nourski <nour.kodeih@gmail.com>
2018-02-19 16:04:04 -0500

    added realtor component to homepage component

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import FeaturedListing from './FeaturedListing'
+import RealtorCard from './RealtorCard'
 
 const HomePage = (props) => {
   const listings = props.listings
+  const realtors = props.realtors
 
   return (
     <div>
       <h1> Featured Listings </h1>
       <div className="scrolling-wrapper-flexbox">
         {listings.map(listing => <FeaturedListing listing = {listing} />)}
+      </div>
+      <h1> Realtors </h1>
+      <div className="scrolling-wrapper-flexbox">
+        {realtors.map(realtor => <RealtorCard realtor = {realtor} />)}
       </div>
     </div>
   )
